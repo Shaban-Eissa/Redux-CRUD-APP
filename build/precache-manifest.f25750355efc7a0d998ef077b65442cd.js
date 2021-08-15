2f434faf7f3e547a4d14e290d1e1779b82238596
unknown <shabaneissa767@gmail.com>
2021-08-16 01:07:29 +0200

    first commit

---
+++ build/precache-manifest.f25750355efc7a0d998ef077b65442cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a907285404b744be75abc45c3a29983",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "./static/js/2.74abc827.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.74abc827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ca2f5371350dbee298",
+    "url": "./static/js/main.bfccc367.chunk.js"
+  },
+  {
+    "revision": "52c409d0048866cab122",
+    "url": "./static/js/runtime-main.7662edb3.js"
+  }
+]);
